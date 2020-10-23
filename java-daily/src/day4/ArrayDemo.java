ca4fc88f2b318bef56fbd63d69c96cb86ef974e6
guoying03 <2834168063@qq.com>
2020-10-23 21:17:55 +0800

    10.23

--- java-daily/src/day4/ArrayDemo.java
+++ java-daily/src/day4/ArrayDemo.java
@@ -1,8 +1,52 @@
-package day4;/**
-  *@ClassName ArrayDemo
-  *@Description TODO
-  *@Author GYY
-  *@Date 2020/10/23
-**/
+package day4;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName ArrayDemo
+ * @Description Arrays练习
+ * @Author GYY
+ * @Date 2020/10/23
+ * java.utill.Arrays 是一个与数组相关的工具类，里面提供了大量静态方法，用来实现数组常见的操作。
+ * public static String toString(数组): 将参数数组变成字符串（按照默认格式：[元素1，元素2，元素3....]
+ *public statc void sort(数组)：按照默认升序（从小到大）对数组的元素进行排序。
+ * 备注：
+ * 1. 如果时数组，sort默认按照升序从小到大
+ * 2. 如果时字符串，sort默认按照字母升序
+ * 3. 如果是自定义的类型，那么这个自定义的类需要有Comparable或者Comparator接口的支持。
+ **/
 public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] intArray = {10,20,30};
+        //将int[]数组按照默认格式变成字符串
+        String inStr = Arrays.toString(intArray);
+        System.out.println(inStr);
+
+        //对整型数组排序
+        int[] array1 = {2, 1, 3, 10, 6};
+        Arrays.sort(array1);
+        System.out.println(Arrays.toString(array1));
+
+        //对字符串数组排序
+        String[] array2 = {"bbb", "aaa", "ccc"};
+        Arrays.sort(array2);
+        System.out.println(Arrays.toString(array2));
+
+        //将一个随机字符串中的所有字符升序排列、并倒序打印
+        String str = "asv76agfqwdfvasdfvjh";
+        //如何进行升序排列：sort
+        //必须是一个数组，才能用Array.sotr方法
+        //String--> 数组，同toCharArray
+        char[] chars = str.toCharArray();
+        //对字符数组进行升序排列‘
+        Arrays.sort(chars);
+        //倒序遍历
+        for(int i = chars.length - 1; i >= 0; i--){
+            System.out.println(chars[i] + " ");
+
+        }
+
+    }
+
 }
+
--- java-daily/src/day4/MyClass.java
+++ java-daily/src/day4/MyClass.java
@@ -1,8 +1,41 @@
-package day4;/**
-  *@ClassName MyClass
-  *@Description TODO
-  *@Author GYY
-  *@Date 2020/10/23
-**/
+package day4;
+
+/**
+ * @ClassName MyClass
+ * @Description 成员变量、静态变量、成员方法、静态方法
+ * @Author GYY
+ * @Date 2020/10/23
+ **/
 public class MyClass {
+    /**
+     * 成员变量
+     */
+    int num;
+    /**
+     * 静态变量
+     */
+    static int numStaic;
+    /**
+     * 成员方法
+     */
+    public void method(){
+        System.out.println("这是一个成员方法。");
+        //成员方法可以访问成员变量
+        System.out.println(num);
+        //成员方法可以访问静态变量
+        System.out.println(numStaic);
+    }
+    /**
+     * 静态方法
+     */
+    public static void methodStatic(){
+        System.out.println("这是一个静态方法。");
+        //静态方法可以访问静态变量
+        System.out.println(numStaic);
+        //静态不能直接访问非静态【重点】
+        //System.out.println(num); //错误写法！
+
+        //静态方法中不能使用this关键字
+        //System.out.println(this);//错误写法！
+    }
 }
