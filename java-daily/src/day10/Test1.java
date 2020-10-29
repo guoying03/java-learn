f78242604082885a64cfc939766d17c75c992706
guoying03 <2834168063@qq.com>
2020-10-29 19:46:55 +0800

    10.29

---
+++ java-daily/src/day10/Test1.java
@@ -0,0 +1,8 @@
+package day10;/**
+  *@ClassName Test1
+  *@Description TODO
+  *@Author GYY
+  *@Date 2020/10/29
+**/
+public class Test1 {
+}
---
+++ java-daily/src/day10/Test2.java
@@ -0,0 +1,8 @@
+package day10;/**
+  *@ClassName Test2
+  *@Description TODO
+  *@Author GYY
+  *@Date 2020/10/29
+**/
+public class Test2 {
+}
